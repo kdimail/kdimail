5eb1f0c73d2b08016c8a6b4f637d30cf4dd9958f
root <root@gitlab.test.tld>
2021-07-03 13:39:55 -0400

    sizer

---
+++ sizer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+my_file_size=$(stat -c%s $1)
+if [ $my_file_size -lt 1024 ];
+ 	then
+ 		echo "OK"
+elif
+[ $my_file_size -gt 1024 ];
+	then
+		echo "FAIL"
+elif
+[ $my_file_size -eq 1024 ];
+	then
+		echo "OK"
+fi
