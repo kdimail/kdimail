fd071c4676926dd5d391688a7ea805ce0c70b697
kdimail <62828900+kdimail@users.noreply.github.com>
2021-12-08 20:45:56 +0200

    Revert "Daniel"

--- sizer.sh
+++ sizer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# git check test
 my_file_size=$(stat -c%s $1)
 if [ $my_file_size -lt 1024 ];
  	then
@@ -11,5 +10,5 @@ elif
 elif
 [ $my_file_size -eq 1024 ];
 	then
-		echo "OK!"
+		echo "OK"
 fi
